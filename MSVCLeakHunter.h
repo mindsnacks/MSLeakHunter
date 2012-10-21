e9c68f6077ced081e4bc71d71700b247ba3d8946
Javier Soto <javier.api@gmail.com>
2012-10-20 17:37:39 -0700

    Initial commit

---
+++ MSVCLeakHunter.h
@@ -0,0 +1,55 @@
+/*
+
+ Copyright 2012 Javier Soto (javi@mindsnacks.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+/**
+ * @discussion set this to 0 to disable this class completely (no code will be compiled).
+ * It's recommended to ship a production app with this flag set to 0.
+ */
+#define MSVCLeakHunter_ENABLED 1
+
+#if MSVCLeakHunter_ENABLED
+
+/**
+ * @discussion this makes MSVCLeakHunter print logs when view controllers appear, disappear and are deallocated.
+ * This helps you track down the lyfe cycle of your view controllers.
+ * `MSVCLeakHunter_ENABLED` has to be set to 1 for this to work too.
+ */
+#define MSVCLeakHunter_EnableUIViewControllerLog 1
+
+/**
+ * @discussion if a view controller hasn't been deallocated after this time after it disappeared from screen, it's considered "pottentially leaked", and a message in the log will be printed. 
+ * Tweak this value depending on the characteristics of your app.
+ */
+#define kMSVCLeakHunterDisappearAndDeallocateMaxInterval 10.0f
+
+/**
+ * @discussion when installed, it's going to print messages in the log whenever a view controller is not deallocated after `kMSVCLeakHunterDisappearAndDeallocateMaxInterval` seconds of disappearing from screen.
+ */
+@interface MSVCLeakHunter : NSObject
+
+/**
+ * @discussion installs the appropiate hooks on UIViewController to start tracking the controllers.
+ * You have to call this method for MSVCLeakHunter to start doing its job.
+ * It's recommended to call this method in your `applicationDidFinishLaunchingWithOptions:` method.
+ */
++ (void)install;
+
+@end
+
+#endif
---
+++ MSVCLeakHunter.m
@@ -0,0 +1,151 @@
+/*
+
+ Copyright 2012 Javier Soto (javi@mindsnacks.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MSVCLeakHunter.h"
+#include <objc/runtime.h>
+
+#if MSVCLeakHunter_ENABLED
+
+#if MSVCLeakHunter_EnableUIViewControllerLog
+    #define MSLOG_METHOD(NAME) NSLog(@"-[%@ %@]", NSStringFromClass([self class]), NAME)
+#else
+    #define MSLOG_METHOD(NAME)
+#endif
+
+@interface UIViewController (MSVCLeakHunter)
+
+- (void)_msvcLeakHunter_viewDidAppear:(BOOL)animated;
+- (void)_msvcLeakHunter_viewDidDisappear:(BOOL)animated;
+- (void)_msvcLeakHunter_dealloc;
+
+@end
+
+@implementation MSVCLeakHunter
+
+/**
+ * @discussion just an instance of the class to schedule the calls to `viewControllerLeakedWithReference:`
+ */
++ (MSVCLeakHunter *)sharedInstance
+{
+    static MSVCLeakHunter *sharedInstance = nil;
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+
+    return sharedInstance;
+}
+
+- (void)viewControllerLeakedWithReference:(NSString *)viewControllerReference
+{
+    NSLog(@"[%@] POSSIBLE LEAK OF VIEW CONTROLLER %@", NSStringFromClass([self class]), viewControllerReference);
+}
+
++ (void)install
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self swizzleMethod:@selector(viewDidAppear:)
+                 withMethod:@selector(_msvcLeakHunter_viewDidAppear:)];
+
+        [self swizzleMethod:@selector(viewDidDisappear:)
+                 withMethod:@selector(_msvcLeakHunter_viewDidDisappear:)];
+
+        [self swizzleMethod:@selector(dealloc)
+                 withMethod:@selector(_msvcLeakHunter_dealloc)];
+    });
+
+}
+
+#pragma mark - Swizzling
+
++ (void)swizzleMethod:(SEL)originalSelector
+           withMethod:(SEL)newSelector
+{
+    Class class = [UIViewController class];
+
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method newMethod = class_getInstanceMethod(class, newSelector);
+
+    method_exchangeImplementations(originalMethod, newMethod);
+}
+
+@end
+
+@implementation UIViewController (MSVCLeakHunter)
+
+/**
+ * @return a string that identifies the controller. This is used to be pased to `MSVCLeakHunter` without retaining the controller.
+ */
+- (NSString *)controllerReferenceString
+{
+    return [NSString stringWithFormat:@"%@ <%p>", NSStringFromClass([self class]), self];
+}
+
+#pragma mark - Alternative method implementations
+
+- (void)_msvcLeakHunter_viewDidAppear:(BOOL)animated
+{
+    MSLOG_METHOD(@"viewDidAppear:");
+
+    [self cancelLeakCheck];
+
+    // Call original implementation
+    [self _msvcLeakHunter_viewDidAppear:animated];
+}
+
+- (void)_msvcLeakHunter_viewDidDisappear:(BOOL)animated
+{
+    MSLOG_METHOD(@"viewDidDisappear:");
+
+    // Call original implementation
+    [self _msvcLeakHunter_viewDidDisappear:animated];
+
+    [self scheduleLeakCheck];
+}
+
+- (void)_msvcLeakHunter_dealloc
+{
+    MSLOG_METHOD(@"dealloc");
+
+    [self cancelLeakCheck];
+
+    // Call original implementation
+    [self _msvcLeakHunter_dealloc];
+}
+
+- (void)cancelLeakCheck
+{
+    [NSObject cancelPreviousPerformRequestsWithTarget:[MSVCLeakHunter sharedInstance]
+                                             selector:@selector(viewControllerLeakedWithReference:)
+                                               object:[self controllerReferenceString]];
+}
+
+- (void)scheduleLeakCheck
+{
+    // Cancel previous ones just in case to avoid multiple calls.
+    [self cancelLeakCheck];
+
+    [[MSVCLeakHunter sharedInstance] performSelector:@selector(viewControllerLeakedWithReference:)
+                                          withObject:[self controllerReferenceString]
+                                          afterDelay:kMSVCLeakHunterDisappearAndDeallocateMaxInterval];
+}
+
+@end
+
+#endif
